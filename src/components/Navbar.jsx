8ba51db8a4433aa34110d8f5b237e17a628e9598
Dániel K. <51174697+DanCodernaut@users.noreply.github.com>
2024-10-20 15:58:13 +0200

    new navbar implemented

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,46 @@
+import { motion } from 'framer-motion';
+import React from 'react';
 import { navLists } from '../constants';
 import { appleImg, bagImg, searchImg } from '../utils';
 
+const MenuItem = ({ item }) => {
+  return (
+    <div className="relative">
+      <motion.p
+        transition={{ duration: 0.3 }}
+        className="cursor-pointer text-gray hover:text-white transition-all"
+      >
+        {item}
+      </motion.p>
+    </div>
+  );
+};
+
+const Menu = ({ children }) => {
+  return (
+    <nav className="relative rounded-3xl border border-transparent dark:bg-black dark:border-white/[0.2] bg-white shadow-input flex justify-center space-x-6 px-12 py-4">
+      {children}
+    </nav>
+  );
+};
+
 const Navbar = () => {
   return (
-    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
-      <nav className="flex w-full screen-max-width">
+    <header className="w-full py-5 sm:px-10 px-5">
+      <div className="flex items-center justify-between w-full max-w-[90rem] mx-auto">
         <img src={appleImg} alt="Apple" width={14} height={18} className="cursor-pointer" />
 
-        <div className="flex flex-1 justify-center max-sm:hidden">
+        <Menu>
           {navLists.map((nav) => (
-            <div
-              key={nav}
-              className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all"
-            >
-              {nav}
-            </div>
+            <MenuItem key={nav} item={nav} />
           ))}
-        </div>
+        </Menu>
 
-        <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1 cursor-pointer">
-          <img src={searchImg} alt="search" width={18} height={18} />
-          <img src={bagImg} alt="bag" width={18} height={18} />
+        <div className="flex items-center gap-4">
+          <img src={searchImg} alt="search" width={18} height={18} className="cursor-pointer" />
+          <img src={bagImg} alt="bag" width={18} height={18} className="cursor-pointer" />
         </div>
-      </nav>
+      </div>
     </header>
   );
 };
